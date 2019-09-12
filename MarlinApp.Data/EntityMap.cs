dd626e5da1b72de9d5799f54dd3e23ea25184f0d
NRunks <renardo@renardoedgecombe.com>
2019-09-12 13:49:32 -0400

    Updated Login Page

---
+++ MarlinApp.Data/EntityMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarlinApp.Data
+{
+    public class UserMap
+    {
+        //public UserMap(EntityTypeBuilder<User> entityBuilder)
+    }
+}
---
+++ MarlinWebApp.Repo/IStoreRepository.cs
@@ -0,0 +1,21 @@
+﻿using MarlinApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarlinWebApp.Repo
+{
+    public interface IStoreRepository : IDisposable
+    {
+        tblUser GetUserByID(int userId);
+        IEnumerable<tblProduct> GetProducts();
+        tblProduct GetProductByID(int productId);
+        tblManufacturer GetProductByManufacturerID(int manufacturerID);
+        void InsertProducts(List<tblProduct> products);
+        void DeleteProduct(int productID);
+        void UpdateProduct(tblProduct product);
+        void Save();
+    }
+}
